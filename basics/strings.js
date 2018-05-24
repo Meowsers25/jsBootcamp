caf745d8fd6e97e48595f636166405b3b97acfce
Chris <c.lamothe25@gmail.com>
2018-05-24 10:02:49 -0400

    Initial commit

---
+++ basics/strings.js
@@ -0,0 +1,16 @@
+// let name = 'Chris'
+// console.log(name)
+
+// let firstName = 'Chris'
+// let lastName = 'McShinovitz'
+// let fullName = firstName + ' ' + lastName
+// console.log(fullName)
+
+//semi colons optional
+
+//city, country, location
+
+let city = 'Providence'
+let country = 'United States'
+let location = city + ', ' + country
+console.log(location)
