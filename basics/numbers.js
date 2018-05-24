c0702980397b3eb3e0cc4a3ddfebea1b795806f9
Chris <c.lamothe25@gmail.com>
2018-05-24 14:09:54 -0400

    numbers

---
+++ basics/numbers.js
@@ -0,0 +1,16 @@
+// let num = 5.5 + 2 // -, *, /, %
+// console.log(num)
+
+//remember order of operations
+//please excuse my dear aunt sally PEMDAS parentheses,exponents,multiplication, division, addition, subtraction//
+// let x = 11
+// let num = x + 1 * 2
+// console.log(num)
+
+//challenge:
+//studentScore, maxScore, percent then print percent
+
+let studentScore = 43
+let maxScore = 50
+let percent = (studentScore / maxScore) * 100
+console.log(percent)
