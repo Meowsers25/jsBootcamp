c34336fcfe15e49dbe2b79009ced14ee316d2942
Chris <c.lamothe25@gmail.com>
2018-05-26 08:02:00 -0400

    More variables

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+let petName = 'Luna'
+// let petName = 'Beatrice'
+//cant define a var more than once; instead reassign 
+petName = 'Beatrice'
+console.log(petName)
+
+//variable names can only begin with a letter(lower or upper), $, or _//
+
+//variable names cannot be resertved keywords
+//let let = 12
