02d404b97215a4c94acac48d4d41db90e1967614
Chris <c.lamothe25@gmail.com>
2018-05-27 19:08:45 -0400

    worked with template strings in the arguments.js file

--- functions/arguments.js
+++ functions/arguments.js
@@ -9,7 +9,8 @@ console.log(result)
 
 //default arguments
 let getScoreText = function (name = 'Anonymous', score = 0) {
-  return 'Name: ' + name + ' -Score: ' + score
+  //return 'Name: ' + name + ' -Score: ' + score
+  return `Name: ${name} -Score: ${score}`
 }
 
 let scoreText = getScoreText('Beatrice')
@@ -19,9 +20,19 @@ console.log(scoreText)
 //Challenge:
 //total, tipPercent
 
+//string template challenge
+//A 15% tip on $55 would be 
 let getTip = function(total, tipPercent = .15) {
-  return 'The tip is: ' + total * tipPercent
+  let percent = tipPercent * 100
+  let tip = total * tipPercent
+  //return 'The tip is: ' + total * tipPercent
+  //you can do math in the {}
+  return `A ${percent}% tip on $${total} would be $${tip}`
 }
 
-let tip = getTip(55.00, .20)
-console.log(tip)
+let answer = getTip(150, .25)
+console.log(answer)
+
+let name = 'Chris'
+let age = 46
+console.log(`Hey my name is ${name}, I am ${age} years old!`)
