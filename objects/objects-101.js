f257df82b085bc7d71656a86ef92c8e4a1b6c97c
Chris <c.lamothe25@gmail.com>
2018-05-29 22:55:53 -0400

    Beginning objects

---
+++ objects/objects-101.js
@@ -0,0 +1,22 @@
+let myBook = {
+  title: '1984',
+  author: 'George Orwell',
+  pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+//challenge
+//name, age, location
+
+let myName = {
+  name: 'Sully',
+  age: 2,
+  location: 'Pawtucket'
+}
+
+console.log(`${myName.name} is ${myName.age} years old and lives in ${myName.location}.`)
