6cacf807319b9d6b5165b48d8cec2adaf994d8b6
Chris <c.lamothe25@gmail.com>
2018-05-30 15:14:12 -0400

    Account expenses tracker challenge

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -4,14 +4,46 @@ let myAccount = {
   income: 0
 }
 
-let otherAccount = myAccount
-otherAccount.income = 1000
+// let otherAccount = myAccount
+// otherAccount.income = 1000
 
 let addExpense = function(account, amount) {
-  
   account.expenses = account.expenses + amount
   //console.log(account)
 }
 
-addExpense(myAccount, 250.00)
-console.log(myAccount)
+//addIncome
+let addIncome = function(account, income) {
+  account.income = account.income + income
+}
+
+//resetAccount
+let resetAccount = function (account) {
+  account.expenses = 0
+  account.income = 0
+}
+
+//getAccountSummary
+let getAccountSummary = function (account) {
+  let balance = account.income - account.expenses
+  return `Account for ${account.name} has $${balance}. $${account.income} in income $${account.expenses} in expenses`
+}
+
+addIncome(myAccount, 2000)
+addExpense(myAccount, 2.50)
+addExpense(myAccount, 160)
+console.log(getAccountSummary(myAccount))
+resetAccount(myAccount)
+console.log(getAccountSummary(myAccount))
+
+//addIncome
+//addExpense
+//addExpense
+//getAccountSummary
+//resetAccount
+//getAccountSummary
+
+
+//addExpense(myAccount, 250.00)
+//console.log(myAccount)
+
