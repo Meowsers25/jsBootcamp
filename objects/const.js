025ed1c1dc959b846df93726826290393e51bb48
Chris <c.lamothe25@gmail.com>
2018-06-05 15:03:10 -0400

    const

---
+++ objects/const.js
@@ -0,0 +1,18 @@
+const isRaining = true
+
+//isRaining = false
+
+console.log(isRaining)
+
+const person = {
+  age: 28
+}
+
+//const person = {}
+
+person.age = 22
+
+console.log(person)
+
+//const helps with code readability; if its a const it tells the reader //that it is a variable that will not change
+
