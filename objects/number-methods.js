ff886184ebfebee451de6ce7f14e9b993bf7cffa
Chris <c.lamothe25@gmail.com>
2018-06-05 14:49:45 -0400

    Number methods

---
+++ objects/number-methods.js
@@ -0,0 +1,25 @@
+let num = 103.941
+
+console.log(num.toFixed(2))
+
+console.log(Math.round(num))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+
+let min = 10
+let max = 20
+let randoNum = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randoNum)
+
+//challenge
+//between 1-5 true if correct; false if incorrect
+
+let makeGuess = function (guess) {
+  let min = 1
+  let max = 5
+  let randoNum = Math.floor(Math.random() * (max - min + 1)) + min
+
+  return guess === randoNum
+}
+
+console.log(makeGuess(3))
