7dd9122f9225e0190d05784333f375e1aa44d49d
Chris <c.lamothe25@gmail.com>
2018-07-05 13:30:12 -0400

    Adds new array directory; Starts work with arrays

---
+++ arrays/todo.js
@@ -0,0 +1,9 @@
+//craete an array with 5 different todos
+//you have x todos; x is length of array
+//print the first and second to last
+
+const todos = ['Walk the pups', 'Feed the cat', 'Feed the pups', 'Go fishing', 'Get some coding done']
+
+console.log(`You have ${todos.length} todos`)
+console.log(`Your first todo: ${todos[0]}`)
+console.log(`Your next to last todo: ${todos[todos.length - 2]}`)
