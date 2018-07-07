8b2725d7e039b23dc77b245ddee9cb729a2b0767
Chris <c.lamothe25@gmail.com>
2018-07-06 21:22:07 -0400

    Uses methods to manipulate arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,16 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
+// notes.push('My new note') // adds new item to end of array
+
+// notes.pop() // removes last item you can also store value in a var
+
+// notes.shift() //removes first item
+
+// notes.unshift('Newest note') // adds item to beginning
+
+//notes.splice(1, 0, 'This is the new second item') // if you use 0 for //the second argument, you can add i an item at that location
+
+notes[2] = 'This is now the new note 3'
+
 console.log(notes.length)
-console.log(notes[0])
+console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -2,8 +2,17 @@
 //you have x todos; x is length of array
 //print the first and second to last
 
+//delete the third item
+//add new item onto the end
+//remove the first item from the list
+
 const todos = ['Walk the pups', 'Feed the cat', 'Feed the pups', 'Go fishing', 'Get some coding done']
 
+todos.splice(2, 1)
+todos.push('Get flat tire fixed')
+todos.shift()
+
 console.log(`You have ${todos.length} todos`)
-console.log(`Your first todo: ${todos[0]}`)
-console.log(`Your next to last todo: ${todos[todos.length - 2]}`)
+console.log(todos)
+// console.log(`Your first todo: ${todos[0]}`)
+// console.log(`Your next to last todo: ${todos[todos.length - 2]}`)
