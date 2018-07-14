4f0ff22c814af65f98da1055d9a004dba54bfdd8
Chris <c.lamothe25@gmail.com>
2018-07-14 07:13:40 -0400

    Uses live-server to render to the web

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,17 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [ 
+  {},
+  {
+    title: 'My Next Trip',
+    body: 'France'
+  },
+  {
+    title: 'Shopping List',
+    body: 'Cat food'
+  },
+  {
+    title: 'Meeting Today',
+    body: '10 AM'
+  }]
 
 // notes.push('My new note') // adds new item to end of array
 
@@ -10,22 +23,28 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 //notes.splice(1, 0, 'This is the new second item') // if you use 0 for //the second argument, you can add i an item at that location
 
-notes[2] = 'This is now the new note 3'
+// notes[2] = 'This is now the new note 3'
 
-notes.forEach(function (note, index) {
-  console.log(index)
-  console.log(note)
-})
+// notes.forEach(function (note, index) {
+//   console.log(index)
+//   console.log(note)
+// })
 
 console.log(notes.length)
 console.log(notes)
 
 //counting......
 //initializer, condition, final expression
-for (let count = 0; count <= 2; count++) {
-  console.log(count)
-}
+// for (let count = 0; count <= 2; count++) {
+//   console.log(count)
+// }
 
-for (let count = 0; count < notes.length; count++) {
-  console.log(notes[count])
-}
+// for (let count = 0; count < notes.length; count++) {
+//   console.log(notes[count])
+// }
+
+//if using index of and the item doesnt exist, it //will return -1
+
+//cant use indexOf with an array of objects
+
+console.log(notes.indexOf({}))
