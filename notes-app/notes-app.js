39a04f86635be516abce30cc3c1842ee816bfc75
Chris <c.lamothe25@gmail.com>
2018-07-14 07:37:33 -0400

    Uses querySelector and querySelectorAll to manipulate the DOM

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,14 @@
-console.log('This is from the js file')
+//DOM
+
+//query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+//query all and remove
+const ps = document.querySelectorAll('p')
+ps.forEach(function(p){
+  p.textContent = '#########'
+  //console.log(p.textContent)
+  //p.remove()
+
+})
